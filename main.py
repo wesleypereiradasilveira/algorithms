eea7f8ae93ae453e06594b67dfbb6c50629c7d12
Wesley Silveira <wesleypereiradasilveira@outlook.com>
2024-02-06 16:19:03 -0300

    added selection sort v2, updated gitignore file, added print original list

--- main.py
+++ main.py
@@ -3,21 +3,23 @@ import numpy as np
 from time import perf_counter
 from rich import print as rprint
 from sorting.bubble_sort import bubble_sort
-from sorting.selection_sort import selection_sort
+from sorting.selection_sort import selection_sort, selection_sort_v2
 from sorting.insertion_sort import insertion_sort
 from sorting.quick_sort import quick_sort
 
 def main():
     rng = np.random.default_rng()
-    # values: list = list(rng.integers(low=-100, high=100, size=20, dtype=int))
-    values: list = [0, 5, 3, 8, -1, 6, 7, 2, 0]
+    values: list = list(rng.integers(low=-100, high=100, size=20, dtype=int))
+    # values: list = [0, 5, 3, 8, -1, 6, 7, 2, 0]
+
+    rprint(f"[blue bold]original list: [/blue bold]: {values}")
 
     start = perf_counter()
 
     # rprint(f"[green bold]bubble sort[/green bold]: {bubble_sort(values)}")
-    # rprint(f"[green bold]selection sort[/green bold]: {selection_sort(values)}")
+    # rprint(f"[green bold]selection sort[/green bold]: {selection_sort_v2(values)}")
     # rprint(f"[green bold]selection sort[/green bold]: {insertion_sort(values)}")
-    rprint(f"[green bold]selection sort[/green bold]: {quick_sort(values)}")
+    # rprint(f"[green bold]selection sort[/green bold]: {quick_sort(values)}")
 
     stop = perf_counter()
     rprint(f"Elapsed time: {(stop - start):.6f}")
--- sorting/selection_sort.py
+++ sorting/selection_sort.py
@@ -12,3 +12,15 @@ def selection_sort(values: list) -> list:
         values[min_position] = temp
 
     return values
+
+def selection_sort_v2(values: list) -> list:
+    size = len(values)
+    for j in range(size - 1):
+        min_index = j
+        for i in range(j, size):
+            if values[i] < values[min_index]:
+                min_index = i
+        
+        values[j], values[min_index] = values[min_index], values[j]
+
+    return values
