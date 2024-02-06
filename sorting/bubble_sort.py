fdd382a5ea3daed4af8ef24b396b006cf821ccaf
Wesley Silveira <wesleypereiradasilveira@outlook.com>
2024-02-06 16:46:07 -0300

    added bubble sort v2

--- sorting/bubble_sort.py
+++ sorting/bubble_sort.py
@@ -9,3 +9,17 @@ def bubble_sort(values: list) -> list:
                 values[j+1] = temp
 
     return values
+
+def bubble_sort_v2(values: list) -> list:
+    n = len(values)
+    for i in range(n - 1, 0, -1):
+        swapped = False
+        for j in range(i):
+            if values[j] > values[j + 1]:
+                values[j], values[j + 1] = values[j + 1], values[j]
+                swapped = True
+
+        if not swapped:
+            break
+
+    return values
