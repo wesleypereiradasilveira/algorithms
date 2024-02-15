63f005449e4d97af78dce4de109c5f1c92241b56
Wesley Silveira <wesleypereiradasilveira@outlook.com>
2024-02-15 15:58:43 -0300

    added binary tree folder, added Node definition, added insert function to test Node class

---
+++ binary_tree/tree.py
@@ -0,0 +1,26 @@
+
+
+class Node:
+    def __init__(self, data) -> None:
+        self.left = None
+        self.right = None
+        self.data = data
+
+    def insert(self, data):
+        if self.data is None:
+            self.data = data
+        else:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Node(data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Node(data)
+                else:
+                    self.right.insert(data)
+
+
+
+
--- main.py
+++ main.py
@@ -2,12 +2,13 @@
 import numpy as np
 from time import perf_counter
 from rich import print as rprint
+from binary_tree.tree import Node
 from sorting.bubble_sort import bubble_sort
 from sorting.selection_sort import selection_sort, selection_sort_v2
 from sorting.insertion_sort import insertion_sort
 from sorting.quick_sort import quick_sort
 
-def main():
+def sortings():
     rng = np.random.default_rng()
     values: list = list(rng.integers(low=-100, high=100, size=20, dtype=int))
     # values: list = [0, 5, 3, 8, -1, 6, 7, 2, 0]
@@ -24,5 +25,21 @@ def main():
     stop = perf_counter()
     rprint(f"Elapsed time: {(stop - start):.6f}")
 
+def binary_trees():
+    root = Node("g")
+    root.insert("c")
+    root.insert("b")
+    root.insert("a")
+    root.insert("e")
+    root.insert("d")
+    root.insert("f")
+    root.insert("i")
+    root.insert("h")
+    root.insert("j")
+    root.insert("k")
+    print(root.left.__dict__)
+    print(root.right.__dict__)
+
 if __name__ == "__main__":
-    main()
+    # sortings()
+    binary_trees()
